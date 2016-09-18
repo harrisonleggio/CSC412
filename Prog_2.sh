5f8ef0a4ec13faa284214cc66e99e861cd37bdca
Harrison Leggio <harrisonleggio@my.uri.edu>
2016-09-18 18:35:23 -0400

    Update Prog_2.sh

--- Prog_2.sh
+++ Prog_2.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-echo "Creating folder" $1
 
 if [ -d $1 ]; then
-    echo "Clearing " $1
     rm -rf $1
 fi
 
 if [ ! -d $1 ]; then
     mkdir -p $1;
-    echo "Creating " $1
 fi
 
 for ((i=1; i<=$2; i++));
